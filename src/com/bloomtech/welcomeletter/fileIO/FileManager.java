b2fdb034e073cdc800bc61617d66feb104329463
jarviswynn123 <jwynn6657@gmail.com>
2024-09-15 17:43:56 -0400

    MT4 done

--- src/com/bloomtech/welcomeletter/fileIO/FileManager.java
+++ src/com/bloomtech/welcomeletter/fileIO/FileManager.java
@@ -1,19 +1,45 @@
 package com.bloomtech.welcomeletter.fileIO;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Scanner;
 
 public class FileManager {
     public FileManager() {
     }
 
-    public String getTextFromFile(String filename) {
+    public String getTextFromFile(String filename) throws IOException {
+        File file = new File("src/resources/" + filename + ".txt");
+
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+
+//        return Files.readString(file.toPath());
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
         String returnString = "";
+
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            returnString += line + "\n";
+            line = bufferedReader.readLine();
+        }
+        bufferedReader.close();
+
 
         return returnString;
     }
 
-    public void writeTextToFile(String outFileName, String text) {
+    public void writeTextToFile(String outFileName, String text) throws IOException {
+        File file = new File("src/resources/out/" + outFileName + ".txt");
+
+
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+        bufferedWriter.write(text);
+        bufferedWriter.close();
 
     }
 }
--- src/com/bloomtech/welcomeletter/services/FormLetterService.java
+++ src/com/bloomtech/welcomeletter/services/FormLetterService.java
@@ -1,6 +1,7 @@
 package com.bloomtech.welcomeletter.services;
 
 import com.bloomtech.welcomeletter.fileIO.FileManager;
+import com.bloomtech.welcomeletter.models.Company;
 import com.bloomtech.welcomeletter.models.Employee;
 
 import java.io.IOException;
@@ -10,9 +11,16 @@ public class FormLetterService {
 
     public void generateWelcomeLetter(Employee employee) throws IOException {
         //TODO: Get template text from resources
+        String output = fileManager.getTextFromFile("LetterTemplate");
 
         //TODO: Replace []ed text with correct info
+        String newOutput = output.replace("[company name]", employee.getCompany().getCompanyname())
+                .replace("[fullname]", employee.getFirstname() + " " + employee.getLastname())
+                .replace("[startdate]", employee.getStartdate().toString())
+                .replace("[startingsalary]", String.valueOf(employee.getSalary()))
+                .replace("[company starttime]", employee.getCompany().getCompanyname());
 
         //TODO: Write finalized String to file
+        fileManager.writeTextToFile("Welcome" + employee.getFirstname() + employee.getLastname(), newOutput);
     }
 }
